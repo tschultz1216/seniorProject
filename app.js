b8c525f511801a50e8083ff1cc8ca307f9f1b08b
Todd Schultz <toddschultz@pop-os.localdomain>
2018-09-01 01:37:50 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 8080;
+
+const voters = [
+
+    {
+        id: '1',
+        name: 'Todd Schultz'
+    },
+    {
+        id: '2',
+        name: 'Ulisse Rotolo'
+    }
+];
+
+app.use((req, res, next) => {
+    console.log(`${req.method} Request Received`);
+    next();
+});
+
+app.listen(PORT, () => console.log(`API listening on ${PORT}`));
+
+app.get('/voters', (res, req, next) => {
+    res.status(200).send(voters);
+});
+
+app.get('/voters/:id', (req, res, next) => {
+    var voterId = req.params.id;
+    var flag = voters.forEach((element) => {
+        if (element.id === voterId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if (flag) {
+        res.status(200).send(element);
+    } else {
+        res.status(404).send();
+    }
+})
