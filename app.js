2a30c144cbb42f401ef85e36e8969d717518f289
Todd Schultz <toddschultz@pop-os.localdomain>
2018-09-01 01:43:28 -0400

    fixed caused by having res before req in callback params

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.listen(PORT, () => console.log(`API listening on ${PORT}`));
 
-app.get('/voters', (res, req, next) => {
+app.get('/voters', (req, res, next) => {
     res.status(200).send(voters);
 });
 
